fd61cefefba94b77f862d1803b4fb7bbff3593be
Winston Shih <80659774+Winstonshih@users.noreply.github.com>
2025-07-03 15:23:46 -0500

    Update populate.py

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -13,7 +13,7 @@ def initiate():
     for data in car_make_data:
         car_make_instances.append(
             CarMake.objects.create(
-                name=data['name'], 
+                name=data['name'],
                 description=data['description']))
     # Create CarModel instances with the corresponding CarMake instances
     car_model_data = [
